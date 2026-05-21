f5f8a3a157fd0cc5a976cc478f057d8aa54c5cc1
dwarakn-gif <dwarakn@gmail.com>
2026-05-21 18:44:28 +1000

    Cleaned up presentation a bit

--- src/components/ClientList.jsx
+++ src/components/ClientList.jsx
@@ -1,7 +1,8 @@
 import { DetailsList } from "@fluentui/react"; import { clients } from "../data";
 export default function ClientList({ setPage }) { const columns = [
 { key: "name", name: "Name", fieldName: "name", minWidth: 150 },
-{ key: "email", name: "Email", fieldName: "email", minWidth: 200 }, ];
+{ key: "email", name: "Email", fieldName: "email", minWidth: 150 },
+{ key: "phone", name: "Phone", fieldName: "phone", minWidth: 100 }, ];
 return ( <div>
 <h2>Clients</h2> <DetailsList
 items={clients}
